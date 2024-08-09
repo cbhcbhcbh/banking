4983767af4513f172c1e8cd60f2cea30388fc7a1
cbhcbhcbh <15862528607@163.com>
2024-08-09 16:00:13 +0800

    Auth Page Complete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,37 @@
 import React from 'react'
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import RightSidebar from '@/components/RightSidebar';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Home = () => {
-  const loggedIn = {firstName: 'Adrian'};
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
 
   return (
     <section className='home'>
       <div className='home-content'>
         <header className='home-header'>
-          <HeaderBox 
+          <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedIn?.name || 'Guest'}
             subtext="Access and manage your account and transactions efficiently."
           />
 
 
           <TotalBalanceBox
-            accounts={ [] }
+            accounts={[]}
             totalBanks={1}
             totalCurrentBalance={1250.35}
           />
         </header>
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50 }]}
+      />
     </section>
   )
 }
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -48,19 +48,27 @@ const MobileNav = ({ user }: MobileNavProps) => {
                                 {sidebarLinks.map((item) => {
                                     const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
                                     return (
-                                        <Link href={item.route} key={item.label}
-                                            className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}
-                                        >
-                                            <div className='relative size-6'>
-                                                <Image src={item.imgURL} alt={item.label} fill className={cn({ 'brightness-[3] invert-0': isActive })} />
+                                        <SheetClose asChild key={item.route}>
+                                            <Link href={item.route} key={item.label}
+                                                className={cn('mobilenav-sheet_close w-full', { 'bg-bank-gradient': isActive })}
+                                            >
 
-                                            </div>
-                                            <p className={cn('sidebar-label', { "!text-white": isActive })}>
-                                                {item.label}
-                                            </p>
-                                        </Link>
+                                                <Image
+                                                    src={item.imgURL}
+                                                    alt={item.label}
+                                                    width={20}
+                                                    height={20}
+                                                    className={cn({ 'brightness-[3] invert-0': isActive })} />
+
+                                                <p className={cn('text-16 font-semibold text-black-2', { "text-white": isActive })}>
+                                                    {item.label}
+                                                </p>
+                                            </Link>
+                                        </SheetClose>
                                     )
                                 })}
+
+                                
                             </nav>
                         </SheetClose>
                     </div>
